27b0e1e64430a423e67df98765cc8ec7f45cbc8a
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-04-16 11:38:50 +0300

    Analysing Data with Modules and Syntax in JS Lab

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-
-
+require('datejs');
+ 
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+ 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+ 
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+ 
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
